154db05e197267661710fcbd7b5db5f001e6c7b4
xdrond <xdrond@gmail.com>
2019-12-12 01:34:47 +0700

    Создание заготовки скрипта пуша кода на робота

--- connects.py
+++ connects.py
@@ -1,18 +1,21 @@
 # python3
-# Набросок для однопоточного сервера.
-import sys
-from socketserver import BaseRequestHandler, TCPServer
 
+from socketserver import BaseRequestHandler, ThreadingMixIn, TCPServer
 
 class MyTCPHandler(BaseRequestHandler):
-    def handle(self):
-        data = self.request.recv(1024).strip()
-        self.request.sendall(data[::-1])
+  def handle(self):
+    data = self.request.recv(1024)
+    self.request.sendall(data[::-1])
+
+
+# Важная строчка! Эквивалент socketserver.ThreadingTCPServer
+# https://github.com/python/cpython/blob/43fc3bb7cf0278735eb0010d7b3043775a120cb5/Lib/socketserver.py#L682
+class ThreadedTCPServer(ThreadingMixIn, TCPServer): pass
 
 
 if __name__ == '__main__':
-    host = 'localhost'
-    port = int(sys.argv[1])
+  host = 'localhost'
+  port = int(sys.argv[1])
 
-    with TCPServer((host, port), MyTCPHandler) as srv:
-        srv.serve_forever()
+  with ThreadedTCPServer((host, port), MyTCPHandler) as srv:
+    srv.serve_forever()
